8f8ae2f2f9d762389d8f6ad679300fae381accf6
Aleksandr Matrosov <unglud@gmail.com>
2021-09-03 19:09:03 +0200

    feat: add context menu

--- src/background.ts
+++ src/background.ts
@@ -2,12 +2,22 @@
   // settings
   await chrome.action.setBadgeBackgroundColor({ color: "green" });
 
+  // context menu
+  const contextMenuAction = (info: chrome.contextMenus.OnClickData) =>
+    console.log(`contextMenuAction`, info);
+
+  chrome.contextMenus.onClicked.addListener(contextMenuAction);
+  chrome.contextMenus.create({
+    id: "context",
+    title: "Remove this",
+  });
+
   // main
   chrome.action.onClicked.addListener(async (tab) => {
     await chrome.action.setBadgeText({ text: "1", tabId: tab.id });
   });
 
-  console.log(`unglued`);
+  console.log(`unglued1`);
 })().catch((e) => {
   console.error(e);
 });
