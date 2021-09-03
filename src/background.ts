028267b68518df7b0a9ae227b641487327742b02
Aleksandr Matrosov <unglud@gmail.com>
2021-09-03 18:40:58 +0200

    fix: cleanup

--- src/background.ts
+++ src/background.ts
@@ -1,2 +1,13 @@
-// This file is ran as a background script
-console.log("Hello from background script!");
+(async () => {
+  // settings
+  await chrome.action.setBadgeBackgroundColor({ color: "green" });
+
+  // main
+  chrome.action.onClicked.addListener(async (tab) => {
+    await chrome.action.setBadgeText({ text: "1", tabId: tab.id });
+  });
+
+  console.log(`unglued`);
+})().catch((e) => {
+  console.error(e);
+});
