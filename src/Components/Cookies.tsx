10d5227bf533d6e6eb8aa45647bc3a97d3fff9ee
Aleksandr Matrosov <unglud@gmail.com>
2021-09-05 20:30:22 +0200

    feat: show attributes

--- src/Components/Cookies.tsx
+++ src/Components/Cookies.tsx
@@ -1,24 +1,60 @@
 import * as React from "react";
 import { useEffect } from "react";
 import CookieConsent from "react-cookie-consent";
+import "../Styles/component.sass";
 
 export const Cookies = () => {
+  const excludedElements = [".pop-yourself"];
+
+  const getElement = (node: Node) => node.firstChild?.parentElement;
+
+  const excluded = (element: HTMLElement) =>
+    excludedElements.some((selectorString) => element.matches(selectorString));
+
   const outlineAddedElement = (node: Node) => {
     if (node.nodeName === "#text") {
       return;
     }
-    const element = node.firstChild?.parentElement;
+    const element = getElement(node);
+
+    if (element && excluded(element)) {
+      console.log(`Element is excluded`, excludedElements, element);
+      return;
+    }
+
     console.log(`New element added:`, element);
-    element!.style.outline = "2px solid red";
+    element!.className += " pop-yourself pop-yourself-outline";
+  };
+
+  const showAttributes = (mutation: MutationRecord) => {
+    const element = getElement(mutation.target);
+    if (element && excluded(element)) {
+      //console.log(`Element is excluded`, excludedElements, element);
+      return;
+    }
+    const changeBox = document.createElement("div");
+    changeBox.className = "pop-yourself pop-yourself-attributes";
+    let attribute =
+      mutation.attributeName === "class" ? "className" : mutation.attributeName;
+    if (!attribute) {
+      return;
+    }
+
+    const previousValue = mutation.oldValue;
+    // @ts-ignore
+    const currentValue = element[attribute];
+    changeBox.innerHTML = `<p>${attribute}: ${previousValue}</p><p>${attribute}: ${currentValue}</p>`;
+    element!.appendChild(changeBox);
   };
 
   const processMutation = (mutation: MutationRecord) => {
-    console.log(`Mutation detected`, mutation);
+    //console.log(`Mutation detected`, mutation);
     switch (mutation.type) {
       case "childList":
         mutation.addedNodes.forEach(outlineAddedElement);
         break;
       case "attributes":
+        showAttributes(mutation);
         break;
     }
   };
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-const webpack = require("webpack");
 const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 
@@ -17,6 +16,17 @@ const config = {
         test: /\.(js|jsx)$/,
         use: "babel-loader",
         exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
       },
       {
         test: /\.css$/,
